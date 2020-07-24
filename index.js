347168690077457ad7bee2e4e040c43a51555caf
Bassel <mail2bassel@yahoo.com>
2020-07-23 20:13:55 -0400

    Added socket id on connection & design notes

--- index.js
+++ index.js
@@ -3,28 +3,57 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
-const DeckModule = require('./DeckModule');
-const Deck = DeckModule.Deck;
+
+// DeckModule's other exports are .Values and .Suits
+const Deck = require('./DeckModule').Deck;
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var players = [];
+
 gameDeck = new Deck();
 gameDeck.shuffle();
-console.log(gameDeck);
+//console.log(gameDeck);
+
+//gameDeck.pop();
+
+io.on('connection', socket => {
+    console.log(socket.id);
+    //console.log(players);
+    players.push(socket.id);
+    console.log(players);
+
+
+
+    socket.on('disconnect', something => {
+        console.log("disconncting...");
+        console.log(something);
+    });
+});
+
+function hit(){
+    /// deal a card to player
+}
+
+// io.on('disconnect', something => {
+//     console.log("disconnecting...");
+//     console.log(something);
+// });
 
 // io.on('connection', socket => {
-//     //console.log(socket);
-//     socket.deck = new Deck.deck();
-//     socket.deck.shuffle();
-//     io.emit('render', socket.deck);
-//     socket.on('join room 1', () => {
-//         socket.join('room 1', () => {
-//             socket.emit('personal');
-//             console.log("A client has joined room 1.");
-//         });
-//     });
+    //console.log("INSIDE IO CONNECTION EVENT LISTENER");
+    // console.log(socket);
+    // socket.deck = new Deck();
+    // socket.deck.shuffle();
+    // io.emit('render', socket.deck);
+    // socket.on('join room 1', () => {
+    //     socket.join('room 1', () => {
+    //         socket.emit('personal');
+    //         console.log("A client has joined room 1.");
+    //     });
+    // });
 // });
 
 // setInterval(function(){
--- public/game.js
+++ public/game.js
@@ -1,28 +1,30 @@
 const socket = io();
+console.log("beans");
+// console.log(socket.id);
 
 function renderDeck(deck){
-    let deckDiv = document.getElementById('deck');
-	for(var i = 0; i < deck.getLength(); i++){
-        console.log("Good morning !>>?");
-		let card = document.createElement("div");
-		let spanRank = document.createElement("span");
-		spanRank.innerHTML = deck.card[i].Value;
-		spanRank.className = "rank";
-		let spanSuit = document.createElement("span");
-		spanSuit.innerHTML = deck.card[i].Suit === "diamonds" ? '&diams;' : '&' + deck.card[i].Suit + ';';
-		spanSuit.className = "suit";
-		card.appendChild(spanRank);
-		card.appendChild(spanSuit);
-        card.className = deck.card[i].Suit === "diamonds" ? `card rank-${deck.card[i].Value} diams` : `card rank-${deck.card[i].Value} ${deck.card[i].Suit}`;
-	    deckDiv.appendChild(card);
-	}
+	let deckDiv = document.getElementById('deck');
+	console.log(deck);
+	// for(var i = 0; i < deck.deck.length; i++){
+    //     console.log("Good morning !>>?");
+	// 	let card = document.createElement("div");
+	// 	let spanRank = document.createElement("span");
+	// 	spanRank.innerHTML = deck.card[i].Value;
+	// 	spanRank.className = "rank";
+	// 	let spanSuit = document.createElement("span");
+	// 	spanSuit.innerHTML = deck.card[i].Suit === "diamonds" ? '&diams;' : '&' + deck.card[i].Suit + ';';
+	// 	spanSuit.className = "suit";
+	// 	card.appendChild(spanRank);
+	// 	card.appendChild(spanSuit);
+    //     card.className = deck.card[i].Suit === "diamonds" ? `card rank-${deck.card[i].Value} diams` : `card rank-${deck.card[i].Value} ${deck.card[i].Suit}`;
+	//     deckDiv.appendChild(card);
+	// }
 }
 
 // Emitters 
 function joinRoom1(){
 	socket.emit('join room 1');
 }
-
 
 // Receivers 
 socket.on('render', deck => {
